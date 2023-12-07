bd2e86f139361311a2e7b3eb8047aa923888d917
Benjamin Istace <bistace@ad-cng.cng.fr>
2023-12-07 14:05:48 +0000

    Update report.rs

--- src/report.rs
+++ src/report.rs
@@ -54,6 +54,7 @@ pub fn print(metrics_vec: &[Metrics]) {
     if fmt[0].ng50_lg50 == "0 (0)" {
         styled_table = styled_table.with(Disable::row(Rows::new(12..15)));
     }
+    println!("{:?}", fmt[0].mean_quality);
     if fmt[0].mean_quality == "0" {
         styled_table = styled_table.with(Disable::row(Rows::new(15..16)));
     }
